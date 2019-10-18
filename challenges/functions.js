f0cfae77382cd717231242c8fae378d74f6c6d24
taterntots <matthew.a.weidner@gmail.com>
2019-10-18 11:38:54 -0400

    Finshed the functions.js assignment

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,29 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Closures are super important because they control what is and isn't in scope for any particlar function, for example, which variables are shared between sibling functions. Simply put, 'nestedFunction' can access the variable 'internal' because closure allows an inner function to access its outer function.
 
 
 const external = "I'm outside the function";
